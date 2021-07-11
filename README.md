3d19895ab425e1519346ebf025d1aea061a0c15a
Marek Ventur <marekventur@gmail.com>
2021-07-11 16:14:25 +0100

    Add test and recovery certificates, resolve vaccine names

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ This app acts as a QR code scanner for EU vaccination passports
 and displays the results below. You can use this to verify the 
 validity of a given vaccine passport.
 
+You can find test codes to scan here: 
+https://github.com/eu-digital-green-certificates/dgc-testdata
+
--- src/result.js
+++ src/result.js
@@ -24,6 +24,41 @@ const toJson = data => {
   });
   return result;
 }
+
+// source: https://ec.europa.eu/health/sites/default/files/ehealth/docs/digital-green-value-sets_en.pdf
+const VALUE_LOOKUP = {
+  // Diseas
+  "840539006": "COVID-19",
+
+  // Tests
+  "LP6464-4": "Nucleic acid amplification with probe detection",
+  "LP217198-3": "Rapid immunoassay",
+
+  // Vaccine/prophylaxis
+  "1119305005": "SARS-CoV-2 antigen vaccine",
+  "1119349007": "SARS-CoV-2 mRNA vaccine",
+  "J07BX03": "covid-19 vaccines",
+
+  // Vaccine medicinal product
+  "EU/1/20/1528": "Comirnaty",
+  "EU/1/20/1507": "Spikevax (previously COVID-19 Vaccine Moderna)",
+  "EU/1/21/1529": "Vaxzevria",
+  "EU/1/20/1525": "COVID-19 Vaccine Janssen",
+
+  // Vaccine manufacturer
+  "ORG-100001699": "AstraZeneca AB",
+  "ORG-100030215": "Biontech Manufacturing GmbH",
+  "ORG-100001417": "Janssen-Cilag International",
+  "ORG-100031184": "Moderna Biotech Spain S.L.",
+  "ORG-100006270": "Curevac AG",
+  "ORG-100013793": "CanSino Biologics",
+  "ORG-100020693": "China Sinopharm International Corp. - Beijing location",
+  "ORG-100010771": "Sinopharm Weiqida Europe Pharmaceutical s.r.o. - Prague location",
+  "ORG-100024420": "Sinopharm Zhijun (Shenzhen) Pharmaceutical Co. Ltd. -Shenzhen location",
+  "ORG-100032020": "Novavax CZ AS",
+  "ORG-100001981": "Serum Institute Of India Private Limited"
+}
+
 const makeHumanReadable = (data, schema) => {
   const result = {};
   data.forEach((value, key) => {   
@@ -39,7 +74,7 @@ const makeHumanReadable = (data, schema) => {
     } else if (value instanceof Map) {
       result[title] = makeHumanReadable(value, getPropertiesByRef(keySchema["$ref"]));
     } else {
-      result[title] = value;
+      result[title] = VALUE_LOOKUP[value] ?? value;
     } 
   });
   return result;
@@ -76,7 +111,7 @@ const parseData = (data) => {
     const tags = { 18: data => data };
 
     /* eslint-disable no-unused-vars */
-    const [ headers1, headers2, payload, ignoreSignature ] = decode(uncompressedData, { tags });
+    const [ headers1, headers2, payload, ignoreSignature ] = decode(uncompressedData, { tags, useMaps: true });
     /* eslint-enable no-unused-vars */
 
     // todo: verify signature
@@ -116,12 +151,9 @@ export const Result = ({
 
 const ResultData = ({ data, humanReadable, code }) => {
   const [showDetails, setShowDetails] = useState(false);
-  const currentDose = data.v?.[0]?.dn;
-  const totalDoses = data.v?.[0]?.sd;
-  const administeredTimestamp = new Date(data.v?.[0]?.dt);
-  const daysAgo = Math.floor((Date.now() - administeredTimestamp) / 1000 / 60 / 60 / 24);
   const isValid = Date.now() < code.expiry;
-  const isFullyProtected = currentDose === totalDoses && daysAgo > 14;
+  console.log(data);
+  
   return <>
     <button className="show-details" onClick={() => setShowDetails(!showDetails)}>
       {showDetails 
@@ -136,8 +168,9 @@ const ResultData = ({ data, humanReadable, code }) => {
       <dt>DoB</dt>
       <dd>{data.dob}</dd>
 
-      <dt>Vaccine</dt>
-      <dd className={isFullyProtected ? "key--ok": "key--bad"}>{currentDose} / {totalDoses} ({daysAgo} days ago)</dd>
+      <ResultVaccineData vaccine={data.v?.[0]} />
+      <ResultRecoveryData recovery={data.r?.[0]} />
+      <ResultTestData test={data.t?.[0]} />
 
       <dt>Country</dt>
       <dd>{code.issuer}</dd>
@@ -148,6 +181,62 @@ const ResultData = ({ data, humanReadable, code }) => {
     {showDetails && <pre className="details">
       {JSON.stringify(humanReadable, null, 2)}
     </pre>}
+  </>
+}
+
+const ResultVaccineData = ({vaccine}) => {
+  if (!vaccine) {
+    return null;
+  }
+  const currentDose = vaccine.dn;
+  const totalDoses = vaccine.sd;
+  const administeredTimestamp = new Date(vaccine.dt);
+  const daysAgo = Math.floor((Date.now() - administeredTimestamp) / 1000 / 60 / 60 / 24);
+  const isFullyProtected = currentDose === totalDoses && daysAgo > 14;
+  const manufacturer = VALUE_LOOKUP[vaccine.ma] ?? vaccine.ma;
+  const product = VALUE_LOOKUP[vaccine.mp] ?? vaccine.mp;
+
+  return <>
+    <dt>Vaccine</dt>
+    <dd>{manufacturer} / {product}</dd>
+
+    <dt>Dose and time</dt>
+    <dd className={isFullyProtected ? "key--ok": "key--bad"}>
+      {currentDose} / {totalDoses} ({daysAgo} days ago)
+    </dd>
+  </>
+}
+
+const ResultRecoveryData = ({recovery}) => {
+  if (!recovery) {
+    return null;
+  }
+  const valid = new Date(recovery.df) < Date.now() && new Date(recovery.du) > Date.now()
+
+  return <>
+    <dt>Recovery</dt>
+    <dd className={valid ? "key--ok": "key--bad"}>
+      Certified from {recovery.df} to {recovery.du}
+    </dd>
+  </>
+}
+
+const ResultTestData = ({test}) => {
+  if (!test) {
+    return null;
+  }
+
+  const testTime = new Date(test.sc);
+  const testPositive = test.tr !== "260415000";
+  const hoursAgo = Math.floor((Date.now() - testTime) / 1000 / 60 / 60);
+  const valid = hoursAgo < 72 || !testPositive;
+
+  return <>
+    <dt>Test</dt>
+    <dd className={valid ? "key--ok": "key--bad"}>
+      {testTime.toISOString().slice(0, 10)} {testTime.toISOString().slice(11, 16)} UTC ({hoursAgo} hours ago) 
+      - {testPositive ? "POSITIVE" : "negative"}
+    </dd>
   </>
 }
 
